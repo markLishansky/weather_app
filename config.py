f88e77aadc97ca8be0eb0143eadf84ac71c60c6b
Mark Lishansky <lishansky@yandex.ru>
2023-10-21 17:37:52 +0300

    get coords refactor + get weather api

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-USE_ROUNDED_COORDS = True
+USE_ROUNDED_COORDS = False
+OPENWEATHER_API = "7549b3ff11a7b2f3cd25b56d21c83c6a"
+OPENWEATHER_URL = (
+    "https://api.openweathermap.org/data/2.5/weather?"
+    "lat={latitude}&lon={longitude}&"
+    "appid=" + OPENWEATHER_API + "&lang=ru&"
+    "units=metric"
+)
--- coordinates.py
+++ coordinates.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from subprocess import Popen, PIPE
 from typing import Literal
-
+import geocoder
 import config
 from exceptions import CantGetCoordinates
 
@@ -11,7 +11,6 @@ class Coordinates:
     longitude: float
 
 def get_gps_coordinates() -> Coordinates:
-    """Returns current coordinates using MacBook GPS"""
     coordinates = _get_whereami_coordinates()
     return _round_coordinates(coordinates)
 
@@ -20,32 +19,33 @@ def _get_whereami_coordinates() -> Coordinates:
     coordinates = _parse_coordinates(whereami_output)
     return coordinates
 
-def _get_whereami_output() -> bytes:
-    process = Popen(["whereami"], stdout=PIPE)
-    output, err = process.communicate()
-    exit_code = process.wait()
-    if err is not None or exit_code != 0:
+def _get_whereami_output() -> str:
+    location = geocoder.ip('me')
+    
+    if location.ok and location.latlng:
+        latitude, longitude = location.latlng
+        output_str = f"Latitude: {latitude}, Longitude: {longitude}"
+        return output_str  # Convert the string to bytes
+    else:
         raise CantGetCoordinates
-    return output
 
-def _parse_coordinates(whereami_output: bytes) -> Coordinates:
-    try:
-        output = whereami_output.decode().strip().lower().split("\n")
-    except UnicodeDecodeError:
+def _parse_coordinates(coordinates_str: str) -> Coordinates:
+    parts = coordinates_str.strip().split(", ")
+    if len(parts) == 2:
+        latitude_str, longitude_str = parts
+        latitude = _parse_float_coordinate(latitude_str.split(":")[1])
+        longitude = _parse_float_coordinate(longitude_str.split(":")[1])
+        return Coordinates(latitude, longitude)
+    else:
         raise CantGetCoordinates
-    return Coordinates(
-        latitude=_parse_coord(output, "latitude"),
-        longitude=_parse_coord(output, "longitude")
-    )
 
 def _parse_coord(
         output: list[str],
-        coord_type: Literal["latitude"] | Literal["longitude"]) -> float:
+        coord_type: Literal["Latitude"] | Literal["Longitude"]) -> float:
     for line in output:
         if line.startswith(f"{coord_type}:"):
-            return _parse_float_coordinate(line.split()[1])
-    else:
-        raise CantGetCoordinates
+            return _parse_float_coordinate(line.split(":")[1].strip())
+    raise CantGetCoordinates
 
 def _parse_float_coordinate(value: str) -> float:
     try:
@@ -64,3 +64,4 @@ def _round_coordinates(coordinates: Coordinates) -> Coordinates:
 
 if __name__ == "__main__":
     print(get_gps_coordinates())
+
